5e2456dd722f773547cfc5ef528ad8f93adf0ae5
Alejo Perea <mpereamejia@gmail.com>
2023-11-14 15:24:08 -0500

    Index Providers

---
+++ Models/PayMode.cs
@@ -0,0 +1,9 @@
+﻿namespace SupermarketWEB.Models
+{
+	public class PayMode
+	{
+		public int Id { get; set; } //Sera la lleve primaria
+		public string Name { get; set; }
+		public string? Observation { get; set; }
+	}
+}
---
+++ Models/Providers.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketWEB.Models
+{
+	public class Provider
+	{
+		public int Id { get; set; } //Sera la lleve primaria
+		public string Name { get; set; }
+		public int ProductId { get; set; }
+
+		public Product Product { get; set; } = default!; // propiedad de navegacion
+	}
+}
